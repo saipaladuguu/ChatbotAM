b4c7d65fe77420701f8b4a9311826b09493aaafd
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2014-10-20 09:13:28 -0600

    Added GUI components

--- src/chatbot/controller/ChatbotAppController.java
+++ src/chatbot/controller/ChatbotAppController.java
@@ -1,6 +1,7 @@
-package chatbot.controller;
+ package chatbot.controller;
 
 import chatbot.model.Chatbot;
+import chatbot.view.ChatbotFrame;
 import chatbot.view.ChatbotView;
 /**
  * Application Controller for the Chatbot String manipulation project.  Responsible for controlling the View and Model packages.
@@ -21,6 +22,14 @@ public class ChatbotAppController
 	 * The startup message for our chatbot application.
 	 */
 	private String startMessage;
+	/**
+	 * The ending message for the Chatbot.
+	 */
+	private String endMessage;
+	/**
+	 * GUI frame for the application.
+	 */
+	private ChatbotFrame baseFrame;
 	
 	/**
 	 * Creates a ChatbotAppController and initializes the associated View and Model objects.
@@ -28,6 +37,7 @@ public class ChatbotAppController
 	public ChatbotAppController()
 	{
 		appView = new ChatbotView(this);
+		baseFrame = new ChatbotFrame(this);
 		notSoCleverBot = new Chatbot("Mr. not so clever");
 		startMessage = "Welcome to the " + notSoCleverBot.getName() +" Chatbot, type in your name.";
 	}
@@ -48,13 +58,13 @@ public class ChatbotAppController
 	{
 		String message = appView.displayChatbotConversations(startMessage);
 	
-		while(!notSoCleverBot.quitChecker(message))
-		{
-			message = notSoCleverBot.processText(message);
-			message = appView.displayChatbotConversations(message);
-		}
-		
-		quit();
+//		while(!notSoCleverBot.quitChecker(message))
+//		{
+//			message = notSoCleverBot.processText(message);
+//			message = appView.displayChatbotConversations(message);
+//		}
+//		
+//		quit();
 	}
 	
 	/**
--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -3,7 +3,9 @@ package chatbot.model;
 import java.util.ArrayList;
 
 /**
- * Model class for the Chatbot project.  The chatbot has checker methods to direct the output messages for the chatbot.
+ * Model class for the Chatbot project. The chatbot has checker methods to
+ * direct the output messages for the chatbot.
+ * 
  * @author Cody Henrichsen
  * @version 1.3 10/2/14
  */
@@ -14,7 +16,8 @@ public class Chatbot
 	 */
 	private String name;
 	/**
-	 * The programmer specified content area.  Used in the contentChecker(String input) method.
+	 * The programmer specified content area. Used in the contentChecker(String
+	 * input) method.
 	 */
 	private String contentArea;
 	/**
@@ -22,13 +25,17 @@ public class Chatbot
 	 */
 	private int numberOfChats;
 	/**
-	 * The list of memes that are used in the Chatbot.  Used in the memeChecker(String input).
+	 * The list of memes that are used in the Chatbot. Used in the
+	 * memeChecker(String input).
 	 */
 	private ArrayList<String> memeList;
-	
+
 	/**
-	 * Creates a Chatbot object with a specified name.  Initializes the total chats to 0. Creates and fills the memeList.
-	 * @param name The name of the chatbot.
+	 * Creates a Chatbot object with a specified name. Initializes the total
+	 * chats to 0. Creates and fills the memeList.
+	 * 
+	 * @param name
+	 *            The name of the chatbot.
 	 */
 	public Chatbot(String name)
 	{
@@ -37,44 +44,49 @@ public class Chatbot
 		contentArea = "";
 		memeList = new ArrayList<String>();
 		fillTheMemeList();
-		//this. means talk to the current class
+		// this. means talk to the current class
 	}
-	
+
 	/**
 	 * Gets the name of the Chatbot.
+	 * 
 	 * @return The current name of the chatbot as a String.
 	 */
 	public String getName()
 	{
 		return name;
 	}
-	
+
 	/**
 	 * Returns the number of chats for this Chatbot.
+	 * 
 	 * @return The current number of chats.
 	 */
 	public int getNumberOfChats()
 	{
 		return numberOfChats;
 	}
-	
+
 	/**
 	 * Sets the name of the chatbot.
-	 * @param name The new name for the Chatbot
+	 * 
+	 * @param name
+	 *            The new name for the Chatbot
 	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-	
+
 	/**
-	 * Increments the chat count for the Chatbot.  Used for tracking the activity of the chatbot.
+	 * Increments the chat count for the Chatbot. Used for tracking the activity
+	 * of the chatbot.
 	 */
 	private void incrementChats()
 	{
 		numberOfChats++;
 	}
-	
+
 	/**
 	 * Fills the contents of the memeList with internet memes.
 	 */
@@ -87,106 +99,140 @@ public class Chatbot
 		memeList.add("toddler fist");
 		memeList.add("kitties!");
 	}
-	
+
 	/**
-	 * Processed the supplied text from the user to provide a message from the Chatbot.
-	 * @param userText The user supplied text.
+	 * Processed the supplied text from the user to provide a message from the
+	 * Chatbot.
+	 * 
+	 * @param userText
+	 *            The user supplied text.
 	 * @return What the Chatbot says because of the supplied input.
 	 */
 	public String processText(String userText)
 	{
 		String processedText = "";
 		incrementChats();
-		
-		if(memeChecker(userText))
+
+		int randomChoice = (int) (Math.random() * 3);
+		if (userText != null)
 		{
-			processedText = "hey, you found a meme: " + userText;
-			processedText += " isn't that cool.";
+			if (randomChoice == 0)
+			{
+				if(stringLengthChecker(userText))
+				{
+					processedText = "that is wayyyyyyyy too long to read :D";
+				}
+				else
+				{
+					processedText = "short messages are short on meaning :P";
+				}
+			}
+			else if (randomChoice == 1)
+			{
+				if(contentChecker(userText))
+				{
+					processedText = "you know that the secret involves: " + userText;
+				}
+				else
+				{
+					processedText = "";
+				}
+			}
+			else
+			{
+				if (memeChecker(userText))
+				{
+					processedText = "hey, you found a meme: " + userText;
+					processedText += " isn't that cool.";
+				}
+				else
+				{
+					processedText = "Boring, that wasn't a meme.";
+				}
+			}
 		}
-		else
-		{
-			processedText = "Boring, that wasn't a meme.";
-		}
-		
+
 		return processedText;
 	}
-	
+
 	/**
-	 * Checks the length of a string based on its length.
-	 * @param input The supplied String object.
+	 * Checks the length of a String for a long String.
+	 * 
+	 * @param input
+	 *            The supplied String object.
 	 * @return Whether the supplied String is greater than 20 characters.
 	 */
-	private boolean stringChecker(String input)
+	private boolean stringLengthChecker(String input)
 	{
 		boolean isTooLong = false;
-		
-		if(input.length() >= 20)
+
+		if (input.length() >= 20)
 		{
 			isTooLong = true;
 		}
-		
+
 		return isTooLong;
 	}
-	
+
 	/**
 	 * Checks if the supplied String contains the content area of the Chatbot.
-	 * @param input The user supplied String.
-	 * @return Whether the Chatbot's special content area is inside the input String.
+	 * 
+	 * @param input
+	 *            The user supplied String.
+	 * @return Whether the Chatbot's special content area is inside the input
+	 *         String.
 	 */
 	private boolean contentChecker(String input)
 	{
 		boolean hasMyContent = false;
-		
-		if(input.contains(contentArea))
+
+		if (input.contains(contentArea))
 		{
 			hasMyContent = true;
 		}
-		
+
 		return hasMyContent;
 	}
-	
+
 	/**
-	 * Checks to see if the supplied user text matches any of the memes in the Chatbot's meme list.
-	 * @param currentText The user supplied text.
+	 * Checks to see if the supplied user text matches any of the memes in the
+	 * Chatbot's meme list.
+	 * 
+	 * @param currentText
+	 *            The user supplied text.
 	 * @return Whether the String matched any of the built in memes.
 	 */
 	private boolean memeChecker(String currentText)
 	{
 		boolean isAMeme = false;
-		
-		for(String currentMeme : memeList)
+
+		for (String currentMeme : memeList)
 		{
-			if(currentMeme.equalsIgnoreCase(currentText))
+			if (currentMeme.equalsIgnoreCase(currentText))
 			{
 				isAMeme = true;
 			}
 		}
-		
-		for(int loopCount = 0; loopCount < memeList.size(); loopCount++)
-		{
-			if(memeList.get(loopCount).equalsIgnoreCase(currentText))
-			{
-				isAMeme = true;
-			}
-		}
-		
+
 		return isAMeme;
 	}
-	
+
 	/**
 	 * Checks for the input "au revoir" to close the app.
-	 * @param input The supplied String value.
+	 * 
+	 * @param input
+	 *            The supplied String value.
 	 * @return Whether or not it is OK to quit.
 	 */
 	public boolean quitChecker(String input)
 	{
 		boolean okToQuit = false;
-		
-		if(input != null && input.equals("au revoir"))
+
+		if (input != null && input.equals("au revoir"))
 		{
 			okToQuit = true;
 		}
-		
+
 		return okToQuit;
 	}
 
